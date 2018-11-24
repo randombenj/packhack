3e1cde2838ae582cdda7e35f480a0834ad525405
fliiiix <de-ch@hotmail.de>
2018-11-24 18:12:17 +0100

    More stuff bro

--- app/__main__.py
+++ app/__main__.py
@@ -7,6 +7,11 @@ from app.dto.PublicGameState import PublicGameState
 from app.dto.PublicPlayer import PublicPlayer
 from app.dto.ReturnDirections import ReturnDirections
 
+last_desired_point = None
+last_desired_point_reached = False
+last_home_point = None
+
+eaten_big_points = 0
 
 @bottle.post('/start')
 def start():
@@ -14,23 +19,26 @@ def start():
 
 @bottle.post('/chooseAction')
 def move():
+
     start = time.time()
     data = PublicGameState(ext_dict=bottle.request.json)
     agent_id = data.agent_id
     our_player = data.publicPlayers[agent_id]
     our_x, our_y = our_player['position']
-    our_y = (len(data.gameField) - our_y) - 1
+    our_y = int((len(data.gameField) - our_y) - 1)
+    our_x = int(our_x)
     game_grid = data.gameField
     game_grid.reverse()
     
-    weighted_game_grid = get_weighted_game_grid(data.gameField)
+    weighted_game_grid = get_weighted_game_grid(data.gameField, agent_id)
 
     from .bstar import PathFinder
 
     print_state_nice(game_grid, our_x, our_y)
     print("OUR X: {}, OUR Y: {}".format(our_x, our_y))
 
-    desired_point = find_point(data.gameField)
+    
+    desired_point = get_next_point(data, game_grid, agent_id, our_x, our_y)
     print("DESIRED POINT", desired_point)
 
     print()
@@ -49,29 +57,96 @@ def move():
     print(time.time()-start)
     return direction
 
-def find_point(game_grid):
+
+def get_next_point(data, game_grid, agent_id, our_x, our_y):
+    global last_desired_point
+    global last_desired_point_reached
+    global last_home_point
+    global eaten_big_points
+ 
+    if last_desired_point is not None:
+        if (our_x) == last_desired_point[0] and (our_y) == last_desired_point[1]:
+            print("Reached and go home")
+            eaten_big_points += 1
+            last_desired_point_reached = True
+            last_desired_point = None
+
+            desired_point = find_point_home(game_grid, agent_id)
+            last_home_point = desired_point
+            return desired_point
+
+        else:
+            return last_desired_point
+    else:
+        if last_desired_point_reached and last_home_point is None:
+            desired_point = find_point_home(game_grid, agent_id)
+            last_home_point = desired_point
+            return desired_point
+        else:
+            if last_desired_point is None and last_home_point is None:
+                desired_point = find_point(data.gameField, "o")
+                last_desired_point = desired_point
+                return desired_point
+
+            if (our_x) == last_home_point[0] and (our_y) == last_home_point[1]:
+                last_home_point = None
+                last_desired_point_reached = False
+
+                if eaten_big_points < 2:
+                    desired_point = find_point(data.gameField, "o")
+                else:
+                    desired_point = find_point(data.gameField, "°")
+
+                last_desired_point = desired_point
+                return desired_point
+            else:
+                return last_home_point
+
+
+
+def find_point_home(game_grid, agent_id):
+    half_of_length = int(len(game_grid[0]) / 2)
+
+    if agent_id == 0:
+        x = half_of_length - 1
+    elif agent_id == 1:
+        x = half_of_length + 2
+
+    for y, rows in enumerate(game_grid):
+        if rows[x] == " ":
+            return (x, y)
+
+
+def find_point(game_grid, search_for):
     half_of_length = int(len(game_grid[0]) / 2)
     for y, rows in enumerate(game_grid):
-        if "o" in rows[half_of_length:]:
-            x = rows[half_of_length:].index("o")
+        if search_for in rows[half_of_length:]:
+            x = rows[half_of_length:].index(search_for)
             return (half_of_length + x, y)
 
 
-def get_weighted_game_grid(game_grid):
+def get_weighted_game_grid(game_grid, agent_id):
+    field_lenght = int(len(game_grid[0]) / 2)
     weighted_game_grid = []
     for rows in game_grid:
         weighted_row = []
-        for col in rows:
-            w = 10
+        for idx,col in enumerate(rows):
+            w = 1
             if col == "%":
                 w = 0
-            elif col == "O":
+            elif col == "o" and compare_based_on_agent(idx, field_lenght, agent_id):
                 w = 5
-            elif col == "o":
+            elif col == "°" and compare_based_on_agent(idx, field_lenght, agent_id):
                 w = 2
             weighted_row.append(w)
         weighted_game_grid.append(weighted_row)
     return weighted_game_grid
+
+def compare_based_on_agent(idx, field_lenght, agent_id):
+    if agent_id == 0:
+        return idx >= field_lenght
+    if agent_id == 1:
+        return idx <= field_lenght
 
 
 def get_direction(x, y):
